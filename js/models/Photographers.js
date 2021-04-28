5f98bce1fb8c0a2dc49cd1a2e90cd832bed584af
AugustinLegrand <legrand.augustin03250@gmail.com>
2021-04-28 15:18:49 +0200

    Correction directory

--- js/models/Photographers.js
+++ js/models/Photographers.js
@@ -11,7 +11,7 @@ export default class Photographers {
                 return data.photographers
             })
             */
-        const data = fetch("api/data.json")
+        const data = fetch("data.json")
         .then(resp => resp.json())
             .then(data => {
                 return data
